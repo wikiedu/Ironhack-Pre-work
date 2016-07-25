561f2e11f689fa8c6665d3ec29825c3794fef6bd
Edu Calderon <eduhand92@gmail.com>
2016-07-25 11:57:28 +0200

    data types

---
+++ dataTypes.rb
@@ -0,0 +1,14 @@
+name = 'mr. Bond'
+capitalized_name = name.capitalize
+puts 'Hello #{capitalized_name}'
+
+flavours = 'chocolate, mint, strawberry, vanilla, caramel, chili'
+flavours_array = flavours.split(', ')
+
+'How many characters are in this string?'.length
+
+'awesome'.include? 'you'
+'awesome'.include? 'me'
+phrase = 'Just kidding, you’re awesome too'
+phrase.include? 'aw'
+
